20c8e141557b0809df3886ec606ecd93791eba8b
DanJingXian <512493288@qq.com>
2018-03-15 11:22:10 +0800

    完成修改个人信息样式

--- src/components/page/mine.vue
+++ src/components/page/mine.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="mine">
     <Menu  @on-select="goMenuView">
-        <MenuGroup title="文章管理">
+        <MenuGroup title="个人信息">
             <MenuItem name="1">
+                 <Icon type="document-text"></Icon>个人信息              
+            </MenuItem>
+        </MenuGroup>
+        <MenuGroup title="文章管理">
+            <MenuItem name="2">
                  &nbsp;<Icon type="document-text"></Icon>我的文章              
             </MenuItem>
-            <MenuItem name="2">
+            <MenuItem name="3">
                 <Icon type="chatbubbles"></Icon>我的收藏
             </MenuItem>
         </MenuGroup>
         <MenuGroup title="其他管理">
-            <MenuItem name="3">
+            <MenuItem name="4">
                 <Icon type="heart"></Icon>城市天气
             </MenuItem>
-            <MenuItem name="4">
+            <MenuItem name="5">
                 <Icon type="heart-broken"></Icon>显示位置
             </MenuItem>
             <!-- <MenuItem name="5">
@@ -23,15 +28,17 @@
         </MenuGroup>
     </Menu>
     <div class="MenuView">
-        <creation v-if="MenuView[1]"></creation>  
-        <collect v-if="MenuView[2]"></collect>
-        <weather v-if="MenuView[3]"></weather>
-        <location v-if="MenuView[4]"></location>
+        <userinfo v-if="MenuView[1]"></userinfo>
+        <creation v-if="MenuView[2]"></creation>  
+        <collect v-if="MenuView[3]"></collect>
+        <weather v-if="MenuView[4]"></weather>
+        <location v-if="MenuView[5]"></location>
     </div>
   </div>
 </template>
 
 <script>
+import userinfo from '@/components/page/mine/userinfo'
 import creation from '@/components/page/mine/creation'
 import collect from '@/components/page/mine/collect'
 import weather from '@/components/page/mine/weather'
@@ -39,15 +46,15 @@ import location from '@/components/page/mine/location'
 export default {
   data () {
       return {
-        MenuView: [true,false,false,false,false]
+        MenuView: [true,false,false,false,false,false]
       }
   },
   components:{
-    creation,collect,weather,location,
+    userinfo,creation,collect,weather,location,
   },
   methods:{
       goMenuView: function(name) {
-        this.MenuView = [false,false,false,false,false];
+        this.MenuView = [false,false,false,false,false,false];
         this.MenuView[name] = true;
       }
   }
---
+++ src/components/page/mine/userinfo.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="userinfo">
+        <Form ref="formValidate" :rules="ruleValidate" :label-width="80">
+            <FormItem label="头像：" prop="city">
+                <Select  placeholder="Select your city">
+                    <Option value="beijing"><img v-lazy='http://oz033lzfm.bkt.clouddn.com/rongrong.jpg' alt=""></Option>
+                    <Option value="shanghai">London</Option>
+                    <Option value="shenzhen">Sydney</Option>
+                </Select>
+            </FormItem>
+            <FormItem label="用户名：" prop="name">
+                <Input  placeholder="Enter your name"></Input>
+            </FormItem>
+            <FormItem label="手机号：" prop="phone">
+                <Input placeholder="Enter your Phone"></Input>
+            </FormItem>
+            <FormItem label="个性签名：" prop="showme">
+                <Input placeholder="Enter your ShowMe"></Input>
+            </FormItem>
+            <FormItem label="性别：" prop="gender">
+                <RadioGroup >
+                    <Radio label="male">Male</Radio>
+                    <Radio label="female">Female</Radio>
+                </RadioGroup>
+            </FormItem>
+            <FormItem>
+                <Button type="primary" @click="handleSubmit('formValidate')">Submit</Button>
+                <Button type="ghost" @click="handleReset('formValidate')" style="margin-left: 8px">Reset</Button>
+            </FormItem>
+    </Form>
+        <!-- <Card style="width:350px">
+        <p slot="title">
+            <Icon type="ios-film-outline"></Icon>
+            Classic film
+        </p>
+        <a href="#" slot="extra" @click.prevent="changeLimit">
+            <Icon type="ios-loop-strong"></Icon>
+            Change
+        </a>
+        <ul>
+            <li><span>用户名: {{ userinfo.userid }}</span></li>
+            <li><span>手机号: {{ userinfo.userphone }}</span></li>
+            <li><span>个性签名: {{ userinfo.usershowme }}</span></li>
+            <li><span>性别: {{ userinfo.usersex }}</span></li>
+        </ul>
+    </Card> -->
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            userinfo:'',
+        }
+    },
+    created(){
+        this.axios.get('http://txspring.cn:8010/findByUserIdDao?userid=' + this.$cookieStore.getCookie('userid')).then(res =>{
+            console.log(res);
+            this.userinfo = res.data;
+        })
+    }
+}
+</script>
+
+<style lang="less">
+
+</style>
+
